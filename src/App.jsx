13e12a58f6dd8f974f08dca22d29d8a34eae6007
monicakodeeswaran32@gmail.com <monicakodeeswaran32@gmail.com>
2025-03-25 12:01:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+
+
+import MainAbout from './About Us/MainAbout'
+import MainContact from './ContactUs/MainContact'
+import MainCourse from "./Course_sec/MainCourse"
+import MainHome from './Home/MainHome'
+import InstituteLogin from './institute_login/InstituteLogin'
+import AdminLogin from "./Institute_login/AdminLogin"
+
+
+function App() {
+  return (
+ <>
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<MainHome/>}></Route> 
+  <Route path="/About" element={<MainAbout/>}></Route>
+  <Route path="/Courses" element={<MainCourse/>}></Route>
+  <Route path="/Contact" element={<MainContact/>}></Route>
+  <Route path="/Institutelogin" element={<InstituteLogin/>}></Route>
+  <Route path="/Adminlogin" element={<AdminLogin/>}></Route>
+</Routes>
+</BrowserRouter>
+ </>
+  )
+}
+
+export default App
+
---
+++ src/Institute_login/AdminLogin.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import logo from "../images/logo.png";
+function AdminLogin() {
+  return (
+     <div className="flex flex-col items-center m-2 p-2 gap-3">
+          <div className="flex flex-col items-center">
+            <img src={logo} alt="" className="w-[50%] lg:w-[60%]" />
+          </div>
+          <div className="flex flex-col items-center gap-5">
+            <h1 className="font-medium text-2xl lg:text-3xl">Admin Login</h1>
+            <input
+              type="text"
+              placeholder="Username"
+              className="border-2 border-gray-300 rounded-md p-2 lg:w-[150%]  lg:p-3"
+            />
+            <input
+              type="text"
+              placeholder="password"
+              className="border-2 border-gray-300 rounded-md p-2 lg:w-[150%]  lg:p-3"
+            />
+            <button className="bg-black text-white p-2 border-2 rounded-md hover:text-black hover:bg-transparent hover:border-black hover:cursor-pointer font-medium px-3 md:w-[50%] lg:w-[150%] lg:text-xl">
+              Login
+            </button>
+            <p className="font-medium hover:underline  hover:cursor-pointer">
+              Login to Institute Account?
+            </p>
+            <p className="font-medium hover:underline  hover:cursor-pointer">
+              Email: info@icvte.com
+            </p>
+            <p className="font-medium hover:underline  hover:cursor-pointer ">
+              Back to Welcome Page
+            </p>
+          </div>
+        </div>
+
+  )
+}
+
+export default AdminLogin
